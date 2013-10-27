bed3c99b3e84b18e7ed52a708edc0b3a191c4fa2
Antonio Álvarez <analca3@gmail.com>
2013-10-28 00:50:02 +0100

    Initial test with key layout and fonts

---
+++ Install-Archlinux-Script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is an easy script for my own use. It reinstalls my Archlinux system faster.
+
+# Set the keyboard layout
+loadkeys es
+
+# Set the font
+setfont Lat2-Terminus16
+
