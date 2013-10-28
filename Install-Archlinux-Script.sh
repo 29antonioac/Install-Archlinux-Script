5116961073688581688c69bd4e3f760b269ed861
Antonio Álvarez <analca3@gmail.com>
2013-10-28 01:11:26 +0100

    First test with basic but complete installation

--- Install-Archlinux-Script.sh
+++ Install-Archlinux-Script.sh
@@ -8,3 +8,21 @@ loadkeys es
 # Set the font
 setfont Lat2-Terminus16
 
+# Set locale and generate it/them
+echo es_ES.UTF-8 UTF-8 > /etc/locale.gen
+locale-gen
+export LANG=es_ES.UTF-8
+
+# Establish an internet connection 
+########
+
+# Preparing storage devices
+########
+
+# Mount the partitions
+mount /dev/sda1 /mnt
+
+# Select a mirror
+echo "Server = http://osl.ugr.es/archlinux/$repo/os/$arch" > /etc/pacman.d/mirrorlist
+pacman -Syy
+pacstrap -i /mnt base
